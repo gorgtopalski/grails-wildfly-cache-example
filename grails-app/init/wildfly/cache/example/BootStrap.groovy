56b52e0b9e8ab1ed8a2788dc6c683794e7be1bbc
Gueorgui Topalski <gueorgui.tzvetoslavov@gmail.com>
2018-05-17 22:02:12 +0200

    feat: use the new timestamp domain

--- grails-app/init/wildfly/cache/example/BootStrap.groovy
+++ grails-app/init/wildfly/cache/example/BootStrap.groovy
@@ -3,14 +3,7 @@ package wildfly.cache.example
 class BootStrap {
 
     def init = { servletContext ->
-
-        def rand = new Random()
-
-        (1..100).each {
-            log.info("Generating some data")
-            def key = new Key(key: it).save()
-            new Value(value: (it * rand.nextDouble()).toString().encodeAsBase64(), key: key).save()
-        }
+        new Time(name: "today", date: new Date()).save()
     }
 
     def destroy = {
